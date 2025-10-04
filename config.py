a2df5d10c80748e32cc05652675251823d7bdee8
NgJinXuan <jinxuanng111@gmail.com>
2025-10-04 14:58:51 +0800

    ss

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # 这里放你的 Telegram API 信息
 # 可以在 https://my.telegram.org 获取
 
-API_IDS = [26615116, 21683403]  
-API_HASHS = ["fc29d68ab5ff965a07813cc1a599968", "98793ea8f5b7b749ca77c17bdcdcc870"]  
-SESSION_FILES = ["accounts/account1", "accounts/account2"]
+API_IDS = [26615116, 21683403, 24855786]  
+API_HASHS = ["fc29d68ab5ff965a07813cc1a599968", "98793ea8f5b7b749ca77c17bdcdcc870","11807258873b030dbc46fdb9aa5130e5"]  
+SESSION_FILES = ["accounts/account1", "accounts/account2","accounts/account3"]
 
 GROUP = "https://t.me/HengOngBet8Official"
 
